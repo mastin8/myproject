a0bd9b7c8acdce0f2d44cd5e7a13c06c968f4280
mastin8 <antonellomastino83@gmail.com>
2022-08-08 16:35:10 +0200

    Prima prova di modifica sincronizzata da Spring qualcosa

--- src/com/test/helloworld.java
+++ src/com/test/helloworld.java
@@ -5,6 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bacioni Bacioni!");
+		System.out.println("L'integrazione con Git è una merda");
 	}
 
 }
